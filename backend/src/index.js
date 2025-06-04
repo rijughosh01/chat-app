fb9533e7f0fab2049d4842ba60ce338967a6ca9f
Riju Ghosh <pg4009355@gmail.com>
2025-06-04 12:48:30 +0530

    Auth

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
