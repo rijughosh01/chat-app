807281e3dc5c6c79455b99a91844cafe1fb3d63f
Riju Ghosh <pg4009355@gmail.com>
2025-06-08 19:12:41 +0530

    Backend error

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "15mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "chat-app-liard-omega-74.vercel.app",
     credentials: true,
   })
 );
