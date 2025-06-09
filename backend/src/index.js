a5f1a213566408f8f8fde4d165e3f1b98b48315d
Riju Ghosh <pg4009355@gmail.com>
2025-06-09 10:23:11 +0530

    Test No5

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,11 +29,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  const distPath = path.join(__dirname, "../frontend/dist");
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.use(express.static(distPath));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(distPath, "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
